cd6b66998646cd083d23b13cb72534cdd627a795
diogo <a21702801@alunos.ulht.pt>
2020-06-26 02:22:24 +0100

    web server final

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MAILGUN_USER: 'postmaster@sandbox7145092385644a54b06f3ca9e3eb9e5d.mailgun.org',
-    MAILGUN_PASS: '8ac0354f79e8548fd81353f99109341d-65b08458-f84556ab'
+    MAILGUN_USER: 'alterar',
+    MAILGUN_PASS: 'alterar'
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,30 +1,27 @@
 //Importar express e Router
 const express = require('express');
 let router = express.Router();
-const pgErrorHandler = require('./pgErrHand');
+
+//Importar cosias para lidar com passwords e gerar tokens
 const bcrypt = require('bcrypt');
 const randomString = require('randomstring');
 
-//Importar a classe Pool
-const {Pool} = require('pg');
+//Importar coisas para fazer autentificação JWT
+const jwt = require('jsonwebtoken')
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname,'../.env')});
+let refreshTokens = [];
 
-//Criar Pool para fazer conexão com a pg database
-const pool = new Pool({
-    user: "DiogoAzevedo",
-    password: "1234",
-    host: "localhost",
-    port: 5432,
-    database: "buddyAbroadDb",
-    max: 20,
-    _connectionTimeoutMillis: 0,
-    idleTimeoutMillis: 10000
-});
+//Importar a api para fazer queries á base de dados
+const pgApi = require('../database/pgApi.js');
+const pgErrorHandler = require('../database/pgErrHand');
 
 //Importar resources para enviar email de verificação
-const mailerApi = require('../misc/mailer/mailer_api');
+const mailerApi = require('../mailer/mailer_api');
+const SERVER_DOMAIN = process.env.SERVER_DOMAIN;
 
-
-router.route('/register').post( async (req,res) =>{
+// Registo
+router.route('/register').post( async (req,res) => {
 
     let registerRequest = {
             first_name: req.body.first_name,
@@ -35,8 +32,7 @@ router.route('/register').post( async (req,res) =>{
             password: req.body.password,
         };
 
-    await findUserByEmail(registerRequest.email, async (err,result) =>{
-        console.log('Entrei em findUserbyEmail')
+    await pgApi.findUserByEmail(registerRequest.email, async (err,result) => {
         if(err !== undefined) {
             res.status(500).send('Server error.');
             return;
@@ -44,33 +40,28 @@ router.route('/register').post( async (req,res) =>{
 
         // Email not yet Registred
         if(result.length === 0){
-            console.log(result.length);
+
             //Hash Password
             registerRequest.password  = await bcrypt.hash(registerRequest.password, 10);
-            //Gerar secret token (32 long)
-            const secretToken = randomString.generate();
-            registerUser(registerRequest.email,registerRequest.first_name,registerRequest.last_name,registerRequest.date_of_birth,registerRequest.gender,registerRequest.password,secretToken, async (err, result) => {
+
+            await pgApi.registerUser(registerRequest.email,registerRequest.first_name,registerRequest.last_name,registerRequest.date_of_birth,registerRequest.gender,registerRequest.password, async (err, result) => {
 
                 if(err) {
                     res.status(500).send('Server Error.');
                 }
                 if(result) {
-                    res.status(200).send({info:"Verification email sent"});
+                    //Cretate info
+                    const userId = result.rows[0].id
 
                     //Email Content
-                    const text = "Click here to verify your email: http://localhost:3000/auth/verify/" + secretToken;
+                    const text = 'Click here to verify your email: ' + SERVER_DOMAIN + '/auth/verify';
 
-                    //Enviar email
+                    //Enviar email (SMTP)
                     //await mailer.sendVerificationEmail('admin@buddyabroad.com', registerRequest.email,'Please verify your email',text);
 
-                    const data = {
-                        from: 'Buddy Abroad <BuddyAbroad@mailgun.org>',
-                        to: registerRequest.email,
-                        subject: 'Verify your account',
-                        text: text
-                    };
-                    mailerApi.sendMailViaApi(data)
-
+                    //Enviar email (API)
+                    res.status(200).send({info:'Sending verification email'});
+                    mailerApi.sendMailViaApi('Buddy Abroad <BuddyAbroad@mailgun.org>', registerRequest.email, 'Verify your account', text, userId, 1)
 
                 }
             })
@@ -80,51 +71,104 @@ router.route('/register').post( async (req,res) =>{
     });
 });
 
-router.route('/verify/:id').get( async (req,res) =>{
-    const token =  req.params.id;
-    console.log(token);
-    verifyUser(token, (err,result) => {
-        if(err) {
+router.route('/resendVerificationEmail').post( async (req,res) => {
+    const email = req.body.email;
+    console.log(email)
+
+    await pgApi.getPassworAndVerifiedByEmail(email, async (err, result) => {
+        if (err) res.status(500).send('Server Error.');
+
+        if (result) {
+            console.log(result.rows[0])
+
+            if (result.rows.length === 0) {
+                res.status(400).send('Email not registered')
+                return;
+            }
+            if (result.rows[0].verified === true) {
+                res.status(400).send('Email already verified')
+                return;
+            }
+
+            if (result.rows[0].verified === false) {
+                const text = 'Click here to verify your email: ' + SERVER_DOMAIN + '/auth/verify';
+                await mailerApi.sendMailViaApi('Buddy Abroad <BuddyAbroad@mailgun.org>', email, 'Verify your account', text, result.id, 1)
+                res.status(200).send({info:'Sending verification email'});
+            }
+
+
+        }
+    })
+})
+
+//Login
+router.route('/login').post( async (req,res) => {
+    const email = req.body.email
+
+    await pgApi.getPassworAndVerifiedByEmail(email, async (err,result) => {
+        if (err) {
             pgErrorHandler.parseError(err);
             res.status(500).send('Server Error.');
         }
-        if(result) {
-            res.status(200).send("Your account was verified");
+
+        if ( result.rows.length === 0) {
+            res.status(400).send('Email is not registered.')
+            return;
         }
+        if (result.rows[0].verified === true) {
+            if (await bcrypt.compare(req.body.password,result.rows[0].password)) {
+
+                const user = { email: req.body.email}
+                const accessToken = generateAccessToken(user)
+                const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+                await pgApi.addRefreshToken(refreshToken, async (err,addResult) => {
+                    if(err) pgErrorHandler.parseError(err)
+                    if (res) {
+                        refreshTokens.push(refreshToken)
+                        console.log('accessToken: ', accessToken)
+                        console.log('refreshToken: ',refreshToken)
+                        res.status(200).send({email:email, accessToken: accessToken, refreshToken: refreshToken})
+                        return
+                    }
+                });
+            } else {
+                res.status(400).send('Wrong Password')
+                return
+            }
+        } else {
+            res.status(400).send('Email is not verified.')
+            return
+        }
+    })
+})
+
+//Gerar Access token
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
+}
+
+//Verificar Email
+router.route('/verify/:id').get( async (req,res) => {
+    const token =  req.params.id;
+    await mailerApi.checkEmailValid(res,token, async () => {
+        await pgApi.verifyUser(token, async (err, result) => {
+            if (err) {
+                pgErrorHandler.parseError(err);
+                res.status(500).send('Server Error.');
+            }
+            if (result) {
+                await pgApi.invalidateEmail(token, (err, result) => {
+                    if (err) {
+                        pgErrorHandler.parseError(err);
+                        res.status(500).send('Server Error.');
+                    }
+                    if (result) {
+                        res.status(200).send('Your account was verified');
+                    }
+                })
+            }
+        })
     })
 });
-
-async function verifyUser(token,callBack) {
-
-    const sql = "UPDATE public.users SET verified= '1'  WHERE secret_token = $1";
-    pool.query(sql, [token], (err, res) => {
-        callBack(err,res)
-    })
-}
-
-async function findUserByEmail(email,callBack) {
-
-    const sql = 'SELECT * FROM users  WHERE email = $1 limit 1';
-    pool.query(sql, [email], (err, res) => {
-
-        if(err !== undefined) {
-            pgErrorHandler.parseError(err);
-            callBack(err,res)
-        }
-        if (res) {
-            //Return rows
-            console.table(res.rows)
-            callBack(err,res.rows)
-        }
-    })
-}
-
-async function registerUser(email,firstName,lastName, dateOfBirth,gender,password,secretToken,callBack) {
-    const sql = 'INSERT INTO users(email, first_name, last_name, gender, birth_date, password, secret_token) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-
-    pool.query(sql,[email,firstName,lastName,gender,dateOfBirth,password,secretToken], (err, res) => {
-            callBack(err,res)
-    })
-}
 
 module.exports = router;
